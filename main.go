370a429dd52ba57b03ace61873782b0262a94959
apple <apple@appledeMacBook-Air.local>
2019-09-25 23:56:42 +0800

    remove strange code

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/petar/GoLLRB/llrb"
 	"pandadb/db"
 )
-
-type my int
-
-func (i my) Less(than llrb.Item) bool {
-	return i < than.(my)
-}
 
 func main(){
 
@@ -23,16 +16,4 @@ func main(){
 	fmt.Println(name)
 
 	panda.Close()
-
-
-	tree := llrb.New()
-	tree.ReplaceOrInsert(my(1))
-	tree.ReplaceOrInsert(my(2))
-	tree.ReplaceOrInsert(my(3))
-	tree.DeleteMin()
-	tree.Delete(my(3))
-	tree.AscendRange(my(0), my(100), func(i llrb.Item) bool {
-		fmt.Println(i)
-		return true
-	})
 }
